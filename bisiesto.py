1ee1e99224b26b706e04b19dab3662b8767ae22f
AlexAntartico <konejin@gmail.com>
2020-11-09 16:13:19 -0600

    final version -I think/hope

--- bisiesto.py
+++ bisiesto.py
@@ -1,9 +1,9 @@
 #!/bin/python3
 
-#dato de entrada - el año que se quiere comprobar
+#input data
 anio = int(input("Cual es el año a verificar?\n"))
 
-#datos de salida - lo que se quiere que el programa regrese
+#output data
 def exito():
     print ("El año ingresado ", anio, " es bisiesto")
     pass
@@ -12,9 +12,7 @@ def fallo ():
     print ("El año ingresado ", anio, " no es bisiesto")
     pass
 
-#algoritmo - si (anio divisble entre 4 y no multiplo de 100  or (divisible entre 400) 
-# entonces -> output
-
+#algo
 def comprobar_anio():
     if ((anio % 4 == 0 ) and (anio % 100 != 0)) or (anio % 400 == 0):
         exito()
